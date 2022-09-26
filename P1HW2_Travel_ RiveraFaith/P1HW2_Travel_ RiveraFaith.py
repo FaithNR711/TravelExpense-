3810d26c0cc1fcd2e85eedb3709a3d7d706b4b67
FaithNR711 <113610726+FaithNR711@users.noreply.github.com>
2022-09-26 14:50:19 -0400

    Travel Expenses

---
+++ P1HW2_Travel_ RiveraFaith/P1HW2_Travel_ RiveraFaith.py
@@ -0,0 +1,12 @@
+#This program calculates and displays travel expenses
+#9/25/2022
+#CTI-110 P1HW2 - Travel Expense
+#Faith Rivera
+
+print('This program calculates and displays travel expenses')
+
+print('Enter Budget')
+
+print('Enter your travel destination')
+
+print("How much do you think you will spend on gas?')
